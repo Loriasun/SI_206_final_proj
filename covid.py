dff3d923276a4ea61f5659538e0c1f6f76715e53
reneeduyqm <96709514+reneeduyqm@users.noreply.github.com>
2022-04-23 23:11:46 -0400

    added calculation but has errors

--- covid.py
+++ covid.py
@@ -11,9 +11,13 @@ def CreateDB(db_name):
     conn = sqlite3.connect(name)
     cur = conn.cursor()
     return cur, conn 
+    
+def drop_table(cur, conn):
+    cur.execute('DROP TABLE IF EXISTS covid')
+    conn.commit()
 
 def create_covid_table(cur, conn):
-    cur.execute('CREATE TABLE IF NOT EXISTS covid (Country TEXT PRIMARY KEY, Cases INTEGER, Deaths INTEGER, Region TEXT )')
+    cur.execute('CREATE TABLE IF NOT EXISTS covid (Country TEXT PRIMARY KEY, Cases INTEGER, Deaths INTEGER, Percentage FLOAT, Region TEXT )')
     conn.commit()
 
 def add_covid(cur, conn):
@@ -32,17 +36,18 @@ def add_covid(cur, conn):
         death_b = temp[2].text
         death = int(death_b.replace(',', ''))
         region = temp[3].text
+        percentage = death / case
         cur.execute(
             """
-            INSERT OR IGNORE INTO covid (Country, Cases, Deaths, Region)
+            INSERT OR IGNORE INTO covid (Country, Cases, Deaths, Percentage, Region)
             VALUES (?, ?, ?, ?)
             """,
-            (country, case, death, region)
+            (country, case, death, percentage, region)
         )
     conn.commit()
 
 def covid_bar_chart(cur,conn):
-    cur.execute('SELECT Deaths, Country FROM covid LIMIT 10')
+    cur.execute('SELECT Percentage, Country FROM covid LIMIT 10')
     conn.commit()
 
     temp = []
@@ -50,20 +55,20 @@ def covid_bar_chart(cur,conn):
     dict = {}
     for t in cur.fetchall():
         temp.append(t[0])
-        values.append(t[1])
-        dict[t[0]] = t[1]
+        values.append(t[3])
+        dict[t[0]] = t[3]
 
     objects = tuple(temp)
     y_pos = np.arange(len(objects))
     fig1, ax1 = plt.subplots(figsize =(23,8))
 
-    plt.bar(y_pos,values,align='center',alpha=1)
-    plt.xticks(y_pos,objects)
+    plt.barh(y_pos,values,align='center',alpha=1)
+    plt.yticks(y_pos,objects)
     
     plt.xlabel('Country')
-    plt.ylabel('Number of Deaths')
-    plt.title('Number of Deaths by Country')
-    plt.savefig('Number_Deaths_by_Country_bar_chart.png')
+    plt.ylabel('Percent of Deaths')
+    plt.title('Percent of Deaths by Country')
+    plt.savefig('Percent_Deaths_by_Country_bar_chart.png')
 
     plt.show()
     return dict
@@ -72,6 +77,7 @@ def covid_bar_chart(cur,conn):
 
 def main():
     cur, conn = CreateDB("covid.db")
+    drop_table(cur, conn)
     create_covid_table(cur,conn)
     add_covid(cur, conn)
     covid_bar_chart(cur, conn)
